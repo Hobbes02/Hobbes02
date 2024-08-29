bccb2215762413fb5f254415fcf9a99772953d55
Hobbes02 <ktudiver@googlemail.com>
2024-08-29 10:16:56 -0400

    added base scripts

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 var pages = ["home", "games", "tools", "design", "contact"];
 
-for(var p of pages) {
-    document.getElementById(p).onclick = function(){onClick(p)};
+function openPage(pageName) {
+    document.getElementById(pageName).style.height = window.innerHeight - (45 * (pages.length - 1));
 }
+
+
+document.getElementById(pages[0]).onclick = function(){onClick(pages[0])};
+document.getElementById(pages[1]).onclick = function(){onClick(pages[1])};
+document.getElementById(pages[2]).onclick = function(){onClick(pages[2])};
+document.getElementById(pages[3]).onclick = function(){onClick(pages[3])};
+document.getElementById(pages[4]).onclick = function(){onClick(pages[4])};
+
+
+var currentPage = "home";
+
 
 function onClick(pageName) {
     console.log(pageName);
+    openPage(pageName);
 }
